c2920b147f4a2d7627a87db404f8bc848a582a92
Pen Peen <ikegami1022.h@gmail.com>
2024-08-10 22:42:33 +0900

    :memo: format page

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -4,12 +4,12 @@ import { within, userEvent, expect } from "@storybook/test";
 import { Page } from "./Page";
 
 const meta = {
-  title: "Example/Page",
+  title: "Template/Page",
   component: Page,
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: "fullscreen",
   },
+  tags: ["autodocs"],
 } satisfies Meta<typeof Page>;
 
 export default meta;
@@ -17,7 +17,6 @@ type Story = StoryObj<typeof meta>;
 
 export const LoggedOut: Story = {};
 
-// More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
 export const LoggedIn: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
