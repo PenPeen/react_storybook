db7df37f50c07d274858362eaf52c4a1900e2eeb
Pen Peen <ikegami1022.h@gmail.com>
2024-08-11 22:14:57 +0900

    :sparkles: FileInputをPresentationalとContainerに分割

--- src/stories/molecules/FileInput/FileInputPresentational.tsx
+++ src/stories/molecules/FileInput/FileInputPresentational.tsx
@@ -1,25 +1,23 @@
-import { ChangeEventHandler, useRef } from "react";
+import { ChangeEventHandler } from "react";
 import "./file-input.css";
 
-type FileInputProps = {
+type FileInputPresentationalProps = {
+  label: string;
+  handleButtonClick: () => void;
+  inputRef: React.RefObject<HTMLInputElement>;
   heading?: string;
-  label?: string;
   message?: string;
   handleFileSelect?: ChangeEventHandler<HTMLInputElement>;
 };
 
-const FileInput: React.FC<FileInputProps> = ({
+const FileInputPresentational: React.FC<FileInputPresentationalProps> = ({
   heading,
-  label = "ファイルをアップロード",
+  label,
   message,
+  handleButtonClick,
   handleFileSelect,
+  inputRef,
 }) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const handleButtonClick = () => {
-    if (inputRef.current === null) return;
-    inputRef.current.click();
-  };
-
   return (
     <>
       <div className="m-file-input">
@@ -54,4 +52,4 @@ const FileInput: React.FC<FileInputProps> = ({
   );
 };
 
-export default FileInput;
+export default FileInputPresentational;
