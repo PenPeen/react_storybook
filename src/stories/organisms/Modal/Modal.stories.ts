cb5057cd89f65adf117093d0e1717056d130fcce
Pen Peen <ikegami1022.h@gmail.com>
2024-08-14 16:09:36 +0900

    :art: Modal

--- src/stories/organisms/Modal/Modal.stories.ts
+++ src/stories/organisms/Modal/Modal.stories.ts
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import parse from "html-react-parser";
-import "./modal.css";
 
 import Modal from "./ModalContainer";
 import { fn } from "@storybook/test";
--- src/stories/organisms/Modal/ModalPresentational.tsx
+++ src/stories/organisms/Modal/ModalPresentational.tsx
@@ -3,6 +3,7 @@ import ReactModal from "react-modal";
 import closeLogo from "@/assets/close.svg";
 import { Button } from "@/stories/atoms/Button/Button";
 import { ModalProps } from "./ModalContainer";
+import styles from "./modal.module.css";
 
 interface ModalPresentationalProps extends ModalProps {
   showModal: boolean;
@@ -27,9 +28,9 @@ const ModalPresentational: React.FC<
   handleCloseModal,
 }) => {
   return (
-    <div className='o-modal'>
+    <div className={styles.o_modal}>
       {isOpenButton && (
-        <span className='o-modal__ok_button'>
+        <span className={styles.o_modal__ok_button}>
           <Button primary handleClick={handleOpenModal} label={openLabel} />
         </span>
       )}
@@ -37,14 +38,18 @@ const ModalPresentational: React.FC<
         isOpen={showModal}
         onRequestClose={handleCloseModal}
         contentLabel='Modal'
-        className='o-modal__contents'
-        overlayClassName='o-modal__overlay'
+        className={styles.o_modal__contents}
+        overlayClassName={{
+          base: styles.o_modal__overlay,
+          afterOpen: styles.o_modal__overlay__open,
+          beforeClose: styles.o_modal__overlay__before_open,
+        }}
         ariaHideApp={false}
       >
         {isCloseButton && (
           <button
             type='button'
-            className='o-modal__close_button'
+            className={styles.o_modal__close_button}
             onClick={handleCloseModal}
           >
             <img src={closeLogo} alt='close button' height={16} width={16} />
@@ -54,14 +59,14 @@ const ModalPresentational: React.FC<
         {children}
 
         {(isCancelButton || isOkButton) && (
-          <div className='o-modal__button_wrapper'>
+          <div className={styles.o_modal__button_wrapper}>
             {isCancelButton && (
-              <span className='o-modal__cancel_button'>
+              <span className={styles.o_modal__cancel_button}>
                 <Button handleClick={handleCloseModal} label={cancelLabel} />
               </span>
             )}
             {isOkButton && (
-              <span className='o-modal__ok_button'>
+              <span className={styles.o_modal__ok_button}>
                 <Button primary handleClick={handleOK} label={okLabel} />
               </span>
             )}
