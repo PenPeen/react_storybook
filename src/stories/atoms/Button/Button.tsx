4e8f6518f08a19e1581df0b4570857446dcf0c87
Pen Peen <ikegami1022.h@gmail.com>
2024-08-14 12:55:37 +0900

    :sparkles: update  Button.tsx

--- src/stories/atoms/Button/Button.tsx
+++ src/stories/atoms/Button/Button.tsx
@@ -6,7 +6,9 @@ interface ButtonProps {
   backgroundColor?: string;
   size?: "small" | "medium" | "large";
   label: string;
-  onClick?: () => void;
+  isRadius?: boolean;
+  customClass?: string[];
+  handleClick?: () => void;
 }
 
 export const Button: React.FC<ButtonProps> = ({
@@ -14,16 +16,27 @@ export const Button: React.FC<ButtonProps> = ({
   size = "medium",
   backgroundColor,
   label,
+  isRadius,
+  customClass,
+  handleClick,
   ...props
 }: ButtonProps) => {
   const mode = primary ? "a-button--primary" : "a-button--secondary";
+  const radius = isRadius && "a-button--radius";
 
   return (
     <button
-      type="button"
-      className={["a-button", `a-button--${size}`, mode].join(" ")}
+      type='button'
+      className={[
+        "a-button",
+        `a-button--${size}`,
+        mode,
+        radius,
+        customClass,
+      ].join(" ")}
       style={{ backgroundColor }}
       {...props}
+      onClick={handleClick}
     >
       {label}
     </button>
